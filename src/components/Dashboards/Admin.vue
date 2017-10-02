0f84bf618e9eabb1f59e99f4b675391f2da5ddb4
Lionel Mann <lionelmann@gmail.com>
2017-10-02 16:06:53 -0400

    added Boards

--- src/components/Dashboards/Admin.vue
+++ src/components/Dashboards/Admin.vue
@@ -12,45 +12,21 @@
 				</div>
 			</div>
 		</section>
-		<section class="section">
-    		<div class="container">
-				<div class="columns">
-					<div class="column is-one-half">
-						<div v-for="post in posts"><b>Project Name:</b> {{post.name}}</div>
-					</div>
-  					<div class="column"></div>
-				</div>
-    		</div>
-		</section>
+		<hype-breeze></hype-breeze>
 		<hype-footer></hype-footer>
 	</div>
 </template>
 
 <script>
-	import axios from 'axios';
+	import Breeze from '../Dashboards/Boards/Breeze.vue';
 	import Footer from '../Shared/Footer.vue';
 	export default {
-		data() {
-			return {
-				posts: []
-			}
-		},
 		components: {
 			'hype-footer': Footer,
-		},
-		created() {
-			axios.get('https://api.breeze.pm/projects.json?api_token=ymzzTy1nnymU7HiyHgPV')
-        	.then(response => {
-				// JSON responses are automatically parsed.
-				this.posts = response.data;
-				console.log(response)
-			}, error => {
-            	console.log(error);
-        	})
+			'hype-breeze': Breeze,
 		}
 	};
 </script>
 
 <style lang="sass" scoped>
-
 </style>
