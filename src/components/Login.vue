5fcd961a04e60ec19027eb9e1eb1162b7546c027
Lionel Mann <lionelmann@gmail.com>
2017-10-02 22:31:33 -0400

    rough homepage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,53 @@
 <template>
-	<div>
-		<h2>Login Homepage</h2>
-	</div>
+<div class="columns">
+  	<div class="column is-two-thirds">  
+    	<h1 class="is-size-1 vcenter has-text-centered">Welcome to the Dashboard</h1>
+  	</div>
+  	<div class="column">
+		<div class="vcenter">
+			<div class="field">
+				<p class="control has-icons-left has-icons-right">
+					<input class="input is-medium" type="email" placeholder="Email">
+					<span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
+					<span class="icon is-small is-right"><i class="fa fa-check"></i></span>
+				</p>
+			</div>
+			<div class="field">
+				<p class="control has-icons-left">
+					<input class="input is-medium" type="password" placeholder="Password">
+					<span class="icon is-small is-left">
+					<i class="fa fa-lock"></i>
+					</span>
+				</p>
+			</div>
+			<div class="field">
+				<p class="control">
+					<button class="button is-medium">
+					Login
+					</button>
+				</p>
+			</div>
+		</div>
+  	</div>
+</div>
 </template>
 
 <script>
-	export default {
-		
-	};
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
+
+.columns {
+	height: 100vh;
+	margin: 0;
+	color: white;
+	background-color: #7957d5;
+}
+
+.vcenter {
+	position: relative;
+ 	top: 50%;
+	transform: translateY(-50%);
+}
 
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
-      }
+      },
     ]
   },
   resolve: {
