3e1a3cc9c1f14303abf9041515f4935cd9608d57
Emily Dela Cruz <delacruz.emily@gmail.com>
2018-01-26 16:14:45 -0500

    Sets up store basics

---
+++ src/store/actions.js
@@ -0,0 +1,8 @@
+import * as types from './mutation-types'
+import axios from 'axios';
+
+export const actions = {
+    getRelatedPosts( context ) {
+        
+    }
+}
---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+export const state = {
+    activeClient: [],
+    activeProject: [],
+    activeUser: [],
+    projectList: [],
+    clientList: [],
+    userList: []
+}
