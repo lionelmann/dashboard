d4711e0ea83c49f7b07b45f8416543bb1bdd5f47
Lionel Mann <lionelmann@gmail.com>
2018-01-27 20:03:28 -0500

    styles, vuex and layout

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -4,34 +4,48 @@
 		<div class="container">
 
 			<!-- BUTTON and FORM FIELD -->
-			<div class="item">
+			<div class="sidebar">
 				 <div class="field">
                     <label class="label">Client Name</label>
                     <div class="control">
                         <input class="input" require type="text" placeholder="Client Name" v-model="clientName">
 						<br>
-						<input class="input" type="text" placeholder="Description" v-model="clientDesc">
+						<textarea class="input" type="text" placeholder="Description" v-model="clientDesc"></textarea>
                     </div>
                 </div>
-				<button class="button is-primary" @click="addClient">Add Client</button>
+			<b-tooltip label="Add New Client" position="is-bottom">
+				<button class="button is-primary" @click="addClient"><i class="fas fa-plus"></i></button>
+			</b-tooltip>
 			</div>
 
 			<!-- CARD -->
+			<div class="cards">
 			<div class="card" v-for="client in clients">
 				<header class="card-header">
-					<p class="card-header-title">Client: {{client.name}}</p>
+					<p class="card-header-title">{{ client.name }}</p>
 				</header>
 				<div class="card-content">
 					<div class="content">
 						{{client.desc}}
 						<br>
-						<time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+						<time datetime="2016-1-1">Created: Jan 27, 2018 </time>
 					</div>
 				</div>
 				<footer class="card-footer">
-					<a href="#" class="card-footer-item">Edit</a>
-					<a href="#" class="card-footer-item" @click="removeClient(client)" >Delete</a>
+					
+					<a href="#" class="card-footer-item">
+						<b-tooltip :label="'Add New Project to ' + client.name" position="is-left" multilined size="is-small"><i class="fas fa-plus-circle"></i></b-tooltip>
+					</a>
+					
+					<a href="#" class="card-footer-item">
+						<b-tooltip :label="'Edit ' + client.name + ' Settings'" position="is-top"><i class="fas fa-pencil-alt"></i></b-tooltip>
+					</a>
+
+					<a href="#" class="card-footer-item" @click="removeClient(client)" >
+						<b-tooltip :label="'Delete ' + client.name" position="is-right"><i class="far fa-trash-alt"></i></b-tooltip>
+					</a>
 				</footer>
+			</div>
 			</div>
 			
 		</div>
@@ -50,19 +64,19 @@
             return {
 				clientName: '',
 				clientDesc: '',
-                clients: []
+				clients: [],
             }
 		},
 		methods: {
             addClient() {
 				if(this.clientName != '' && this.clientDesc != '') {
-					this.clients.push({
+					this.clients.unshift({
 						name: this.clientName,
 						desc: this.clientDesc
 						}); 
 					this.clientName = '';
 					this.clientDesc = '';
-					console.log(this.clients)
+					//console.log(this.clients)
 				}
             },
             removeClient(client) {
@@ -70,7 +84,7 @@
 				if(i != -1) {
 					this.clients.splice(i, 1);
 				}
-		    },
+			},
         }
 	};
 </script>
@@ -80,13 +94,31 @@
 .container {
 	margin: 20px auto;
 	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+	grid-template-columns: 200px 1fr;
+	grid-template-areas: "sidebar content" "sidebar .";
+	grid-gap: 20px;
+}
+
+.sidebar {
+	grid-area: sidebar;
+}
+
+.cards {
+	display: grid;
+	grid-area: content;
+	grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
 	grid-gap: 20px;
 }
 
 button {
 	height: 100px;
 	width: 80px;
+}
+
+time {
+	display: block;
+	font-size: .815rem;
+	margin-top: 1rem;
 }
 
 
